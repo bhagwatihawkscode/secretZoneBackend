d40d4d549943fda00668c7d3f1a03498597a435c
bhagwatihawkscode <bhagwatilalmenariya.hawkscode@gmail.com>
2023-11-16 14:31:12 +0530

    deploy

--- helper/ProfileHelp.js
+++ helper/ProfileHelp.js
@@ -14,41 +14,49 @@ const uploadProfile = async (customPath, base64Image) => {
   const uploadpath = `public/${customPath}`;
 
   return new Promise(async (resolve, reject) => {
-    if (!fs.existsSync(uploadpath)) {
-      fs.mkdirSync(uploadpath, { recursive: true }); // Create directories recursively
-    }
-
-    const uniqueFilename = `${new Date().getTime()}.png`;
-    const filename = `${uploadpath}${uniqueFilename}`;
-
-    let m = base64Image.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
-    let b = Buffer.from(m[2], "base64");
-
-    fs.writeFile(filename, b, async (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        try {
-          const cloudinaryResponse = await cloudinary.uploader.upload(filename);
-          const cloudinaryUrl = cloudinaryResponse.secure_url;
-
-          // Extract the part of the Cloudinary URL after /image/upload/
-          const relativePath = cloudinaryUrl.replace(
-            "https://res.cloudinary.com/dsvlrlr51/image/upload/",
-            ""
-          );
-
-          // Resolve the promise with the relative path
-          resolve(relativePath);
-        } catch (cloudinaryError) {
-          console.error("Error uploading to Cloudinary:", cloudinaryError);
-          reject(cloudinaryError);
-        } finally {
-          // Clean up: Delete the local file after Cloudinary upload
-          fs.unlinkSync(filename);
-        }
+    try {
+      if (!fs.existsSync(uploadpath)) {
+        fs.mkdirSync(uploadpath, { recursive: true }); // Create directories recursively
       }
-    });
+
+      const uniqueFilename = `${new Date().getTime()}.png`;
+      const filename = `${uploadpath}${uniqueFilename}`;
+
+      let m = base64Image.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+      let b = Buffer.from(m[2], "base64");
+
+      fs.writeFile(filename, b, async (err) => {
+        if (err) {
+          console.error("Error writing file:", err);
+          reject("Error writing file");
+        } else {
+          try {
+            const cloudinaryResponse = await cloudinary.uploader.upload(
+              filename
+            );
+            const cloudinaryUrl = cloudinaryResponse.secure_url;
+
+            // Extract the part of the Cloudinary URL after /image/upload/
+            const relativePath = cloudinaryUrl.replace(
+              "https://res.cloudinary.com/dsvlrlr51/image/upload/",
+              ""
+            );
+
+            // Resolve the promise with the relative path
+            resolve(relativePath);
+          } catch (cloudinaryError) {
+            console.error("Error uploading to Cloudinary:", cloudinaryError);
+            reject("Error uploading to Cloudinary");
+          } finally {
+            // Clean up: Delete the local file after Cloudinary upload
+            fs.unlinkSync(filename);
+          }
+        }
+      });
+    } catch (error) {
+      console.error("Error in uploadProfile:", error);
+      reject("Internal server error");
+    }
   });
 };
 
