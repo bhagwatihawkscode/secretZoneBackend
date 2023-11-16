bb3d99e7f6fb834a75e2ad33c4d8d76eafe9e9af
bhagwatihawkscode <bhagwatilalmenariya.hawkscode@gmail.com>
2023-11-16 19:18:46 +0530

    update

--- helper/ProfileHelp.js
+++ helper/ProfileHelp.js
@@ -11,37 +11,53 @@ cloudinary.config({
 });
 
 const uploadProfile = async (customPath, base64Image) => {
-  try {
-    // Upload to temporary storage (modify as needed)
-    const temporaryStoragePath = "/tmp/"; // Use an in-memory storage or a temporary directory
-    const uniqueFilename = `${new Date().getTime()}.png`;
-    const temporaryFilePath = `${temporaryStoragePath}${uniqueFilename}`;
+  const uploadpath = `public/${customPath}`;
 
-    let m = base64Image.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
-    let b = Buffer.from(m[2], "base64");
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (!fs.existsSync(uploadpath)) {
+        fs.mkdirSync(uploadpath, { recursive: true }); // Create directories recursively
+      }
 
-    fs.writeFileSync(temporaryFilePath, b, "base64");
+      const uniqueFilename = `${new Date().getTime()}.png`;
+      const filename = `${uploadpath}${uniqueFilename}`;
 
-    // Upload to Cloudinary
-    const cloudinaryResponse = await cloudinary.uploader.upload(
-      temporaryFilePath
-    );
-    const cloudinaryUrl = cloudinaryResponse.secure_url;
+      let m = base64Image.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+      let b = Buffer.from(m[2], "base64");
 
-    // Clean up: Delete the temporary file
-    fs.unlinkSync(temporaryFilePath);
+      fs.writeFile(filename, b, async (err) => {
+        if (err) {
+          console.error("Error writing file:", err);
+          reject("Error writing file");
+        } else {
+          try {
+            const cloudinaryResponse = await cloudinary.uploader.upload(
+              filename
+            );
+            const cloudinaryUrl = cloudinaryResponse.secure_url;
 
-    // Extract the part of the Cloudinary URL after /image/upload/
-    const relativePath = cloudinaryUrl.replace(
-      "https://res.cloudinary.com/dsvlrlr51/image/upload/",
-      ""
-    );
+            // Extract the part of the Cloudinary URL after /image/upload/
+            const relativePath = cloudinaryUrl.replace(
+              "https://res.cloudinary.com/dsvlrlr51/image/upload/",
+              ""
+            );
 
-    return relativePath;
-  } catch (error) {
-    console.error("Error in uploadProfile:", error);
-    throw new Error("Internal server error");
-  }
+            // Resolve the promise with the relative path
+            resolve(relativePath);
+          } catch (cloudinaryError) {
+            console.error("Error uploading to Cloudinary:", cloudinaryError);
+            reject("Error uploading to Cloudinary");
+          } finally {
+            // Clean up: Delete the local file after Cloudinary upload
+            fs.unlinkSync(filename);
+          }
+        }
+      });
+    } catch (error) {
+      console.error("Error in uploadProfile:", error);
+      reject("Internal server error");
+    }
+  });
 };
 
 export default uploadProfile;
