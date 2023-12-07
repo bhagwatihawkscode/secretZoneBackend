b5901b222fe0d815e617f9938e51a06ebaed248e
bhagwatihawkscode <bhagwatilalmenariya.hawkscode@gmail.com>
2023-12-07 12:44:13 +0530

    deploy check

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use(
 
 app.use(express.static("public"));
 
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 
 import registerRoute from "./Routes/userRouts.js";
 app.use("/api/todo", registerRoute);
